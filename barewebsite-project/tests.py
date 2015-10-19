d60ffa3f6d5916074900ffbde8908b200584237d
VladKh <khapaloff@gmail.com>
2015-10-19 22:50:37 +0300

    Added some unit tests

--- barewebsite-project/tests.py
+++ barewebsite-project/tests.py
@@ -1,7 +1,20 @@
 from unittest import TestCase
-from barewebsite.core import get_message
+from barewebsite import core
 
 class BarewebsiteTestCase(TestCase):
-    def test_barewebsite(self):
-    self.assertEqual('1', '1')
-#	self.assertEqual(method_to_test, value)
+    def test_create_parser(self):
+        parser = core.create_parser()        
+        self.assertEqual(str(type(parser)), "<class 'argparse.ArgumentParser'>")
+    def test_create_cleaner(self):
+        cleaner = core.create_cleaner()
+        self.assertEqual(str(type(cleaner)), "<class 'lxml.html.clean.Cleaner'>")
+    def test_get_page(self):
+        url = 'http://www.google.com/'
+        page = core.get_page(url)
+        self.assertEqual(str(type(page)), "<class 'lxml.html.HtmlElement'>")
+    def test_get_bare_page(self):
+        url = 'http://www.google.com/'
+        cleaner = core.create_cleaner()
+        page = core.get_page(url)
+        bare_page = cleaner.clean_html(page)
+        self.assertEqual(str(type(page)), "<class 'lxml.html.HtmlElement'>")
